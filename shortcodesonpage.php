339cf9bb9a32097b3a88b30f2740baa356ab1f9f
Martin <154801774+creatorsofcode@users.noreply.github.com>
2024-03-18 15:10:45 +0200

    Add files via upload

---
+++ shortcodesonpage.php
@@ -0,0 +1,10 @@
+	[wc_custom_role_content role="customer"]
+<div id='customer'>Not sign in</div>
+[/wc_custom_role_content]	
+
+
+[wc_custom_role_content role="premium"]
+<div id='premium'>Premium</div>
+[/wc_custom_role_content]
+
+[if_role role="premium"]
